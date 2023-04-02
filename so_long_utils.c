5937d1181f8fa3d6a0049d0597dae8f6dd33e820
sdiaz-ru <sdiaz-ru@studient.42malaga.com>
2023-04-02 18:15:31 +0200

    Añadido itoa

--- so_long_utils.c
+++ so_long_utils.c
@@ -6,7 +6,7 @@
 /*   By: sdiaz-ru <sdiaz-ru@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 15:55:45 by sdiaz-ru          #+#    #+#             */
-/*   Updated: 2023/03/31 10:39:02 by sdiaz-ru         ###   ########.fr       */
+/*   Updated: 2023/04/02 18:14:27 by sdiaz-ru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,52 @@ int	ft_memcmp(char *str1, char *str2, int n)
 	while (str1[i] == str2[i] && i < n - 1)
 		i++;
 	return (str1[i] - str2[i]);
+}
+
+char	*ft_loadstr(long n, long size)
+{
+	char		*str;
+
+	str = (char *)malloc((size + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	str[size--] = '\0';
+	if (n < 0)
+	{
+		str[0] = '-';
+		n *= -1;
+	}
+	while (size != -1 && str[size] != '-')
+	{
+		str[size--] = (n % 10) + '0';
+		n /= 10;
+	}
+	return (str);
+}
+
+char	*ft_itoa(long n)
+{
+	long		size;
+	long		aux;
+	char		*str;
+
+	size = 0;
+	aux = n;
+	if (n == 0)
+	{
+		str = (char *)malloc((2) * sizeof(char));
+		if (str == NULL)
+			return (NULL);
+		str[0] = '0';
+		str[1] = '\0';
+		return (str);
+	}
+	if (n < 0)
+		size++;
+	while (aux != 0)
+	{
+		aux = aux / 10;
+		size++;
+	}
+	return (ft_loadstr(n, size));
 }
